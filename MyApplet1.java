ca5a33e019a026598293230146b497bedeb0fd21
sekhar139 <161412839+sekhar139@users.noreply.github.com>
2025-03-29 12:53:58 +0530

    Add files via upload

---
+++ MyApplet1.java
@@ -0,0 +1,12 @@
+import java.applet.*;
+import java.awt.*;
+/*<applet code="MyApplet1.class" width="400" height="400">
+</applet>*/
+public class MyApplet1 extends Applet
+{
+public void paint(Graphics g)
+{
+g.drawString("Hello World",400,400);
+}
+}
+
